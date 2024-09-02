121969e4c3a1f772d4d9a324fc78632920a1e888
alkadabna <abd-ellah.hioun@laplateforme.io>
2024-09-02 09:28:15 +0200

    the public function

--- job1/job1.php
+++ job1/job1.php
@@ -9,13 +9,24 @@ class Product{
     private DateTime $createdAt;
     private DateTime $updatedAt;
 
+    public function __construct($id, $name, $photos, $price, $description, $quantity, $createdAt, $updatedAt){
+        $this->id = $id;
+        $this->name = $name;
+        $this->photos = $photos;
+        $this->price = $price;
+        $this->description = $description;
+        $this->quantity = $quantity;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+    }
+
+
+
+
+
+
 }
   
-
-
-
-
-
 
 
 
