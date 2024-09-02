c82ed77272081ae47979ef0707f32f936e159c54
alkadabna <abd-ellah.hioun@laplateforme.io>
2024-09-02 09:26:58 +0200

    first classes

---
+++ job1/job1.php
@@ -0,0 +1,23 @@
+<?php
+class Product{
+    private int $id;
+    private string $name;
+    private array $photos;
+    private int $price;
+    private string $description;
+    private int $quantity;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+
+}
+  
+
+
+
+
+
+
+
+
+
+?>
